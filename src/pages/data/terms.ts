3a43e1f801fcb0945f8a668f8aaaee13a5835fb8
Alyxia Sother <lexisoth2005@gmail.com>
2021-06-20 12:00:19 +0000

    [Basic Terms] Reworked rendering using List

---
+++ src/pages/data/terms.ts
@@ -0,0 +1,127 @@
+import {Data} from "../../shared/List";
+
+export const test: Data[] = [
+    {
+        name: "Alter",
+        desc: "The clinical term for someone in a system."
+    },
+    {
+        name: "Blurring",
+        desc: "When two or more people are fronting, and the line between who they are gets blurred / When two people mix up a little and get blurred together."
+    },
+    {
+        name: "Co-Fronting",
+        desc: "Co-fronting or co-hosting is when two or more people are taking control of the body at once."
+    },
+    {
+        name: "Co-Conscious",
+        desc: "When two or more people are conscious at the same time. You're usually able to communicate with each other like this."
+    },
+    {
+        name: "Exotrauma",
+        desc: "Trauma that's specific to one person. Whether it's the mind trying to work through the body's trauma in chunks, or it's trauma from their source if they're an introject. This can also be trauma from other headmates, including persecutors."
+    },
+    {
+        name: "Forming",
+        desc: "When a new person is formed. Can relate to both non-voluntary, and voluntary forming. Can also be called Splitting."
+    },
+    {
+        name: "Fronting",
+        desc: "When someone is in control of the body."
+    },
+    {
+        name: "Front Sticky",
+        desc: "When someone is continuously drawn to front, no matter how many times they leave. Usually, it feels like the body wants them in control."
+    },
+    {
+        name: "Front Stuck",
+        desc: "When someone is unable to stop fronting, and is forced to keep control of the body. Unlike sticky, they can't leave front at all."
+    },
+    {
+        name: "Fusion",
+        desc: "When two or more system members fuse to become one person for a non-permanent amount of time."
+    },
+    {
+        name: "Headmate",
+        desc: "A non-clinical term for someone in a system."
+    },
+    {
+        name: "Innerworld/Headspace",
+        desc: "The innerworld, or headspace, is where headmates live inside the mind. Not all systems have headspace.",
+        link: "innerworld"
+    },
+    {
+        name: "Integration",
+        desc: "In most cases, means when system mates start fusing together permanently, usually to make a system smaller. Sometimes, it can also mean when amnesiac systems stop losing memories between each other. <br /> This can be highly triggering for some systems, and even remind them of death. It's best to be careful on who you talk about it with."
+    },
+    {
+        name: "Kin",
+        desc: "This term is not for only systems, and it doesn't mean system. Kin are people who identify as partially or entirely nonhuman- sometimes just as another person. Whether of a fictional character, a fictional species, a non-fictional species, even another person. Kin =/= Introject. People in a system can kin, but that does not mean kinning is the same as being an introject. (See Member Types)."
+    },
+    {
+        name: "NPC",
+        desc: "A non-alter/headmate in a system. Not real people, but they're filler so headspace isn't so empty."
+    },
+    {
+        name: "Outerworld/Meatspace",
+        desc: "The opposite of headspace, this is the body's world. A system member who is a different ethnicity than the body."
+    },
+    {
+        name: "Parethnic",
+        desc: "A system member who is a different ethnicity than the body.",
+        url: "https://theluminhearts.tumblr.com/post/614953009350426624/new-term-parethnic"
+    },
+    {
+        name: "Partner System",
+        desc: "When two or more systems have multiple relationships between their system mates. Doesn't have to all be romantic."
+    },
+    {
+        name: "Plurality/Multiplicity",
+        desc: "The state of being plural or multiple; the name of having multiple people in one body."
+    },
+    {
+        name: "Pluran",
+        desc: "Similar to headmate, but without defining someone by their relationship to the others in their system."
+    },
+    {
+        name: "Plurisex",
+        desc: "Having different or inhuman sex characteristics because you're in a system."
+    },
+    {
+        name: "Proxy",
+        desc: "Using a Discord Bot (PK, Tupper, etc) to send a message with a webhook of yourself, or another system member relaying your message to other people."
+    },
+    {
+        name: "Shadowing",
+        desc: "Being conscious and (at least somewhat) associated with the body, but not fronting. Metaphorically looking over the fronter's shoulder."
+    },
+    {
+        name: "Singlet",
+        desc: "A non-system. One person with one body."
+    },
+    {
+        name: "Singletsona",
+        desc: "The mask everyone agrees to act as when they aren't out as a system."
+    },
+    {
+        name: "Switching",
+        desc: "When people in system switch who is fronting."
+    },
+    {
+        name: "System",
+        desc: "The collective of people or facets inside the body is (most of the time) referred to as the system."
+    },
+    {
+        name: "System Hopping",
+        desc: "When a system member 'hops' from one system to another, possibly for a visit or to stay forever. While debated, it's really up to every system themselves whether they believe it or not."
+    },
+    {
+        name: "System Stagnant",
+        desc: "When the system starts to get quieter, and it’s harder for people to take front or talk to people outside of system (some even inside system). Kind of like the entire system is dissociating, and only a handful of people can really front, making them frequent fronters."
+    },
+    {
+        name: "You&",
+        desc: "A way to say 'you' while acknowledging the other person is a system. Can work with other terms; I&, Me&, etc.",
+        link: "you"
+    }
+];
---
+++ src/shared/List.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import reactStringReplace from "react-string-replace";
+import {FiLink} from "react-icons/fi";
+
+const ListItem = ({item}: {item: Data}): JSX.Element => {
+    return (
+        <>
+            <div className="link" id={item.link ? item.link : item.name.toLowerCase().replace(" ", "-")}>
+                <a className="linkIcon" href={`#${item.link ? item.link : item.name.toLowerCase().replace(" ", "-")}`}>
+                    <FiLink />
+                </a>{" "}
+                <strong>{item.name}</strong> :{" "}
+                {reactStringReplace(item.desc, "<br />", () => (
+                    <br />
+                ))}{" "}
+                {item.url && <a href={item.url}>♡</a>}
+            </div>
+            <br />
+        </>
+    );
+};
+
+export default function List({list}: {list: unknown[]}): JSX.Element {
+    return (
+        <ul>
+            {/* If there's no list, provide an empty array */}
+            {(list || []).map((item: any) => (
+                <ListItem item={item} />
+            ))}
+        </ul>
+    );
+}
+
+export interface Data {
+    name: string;
+    desc: string;
+    link?: string;
+    url?: string;
+}
